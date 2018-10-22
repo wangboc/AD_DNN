797e5d32a717a929dc0877c35bb0362a8a1cf880
wangboc <baixibao@gmail.com>
2018-10-22 09:17:23 +0800

    30% 左右的识别率

--- AD_DNN_recognition.py
+++ AD_DNN_recognition.py
@@ -14,9 +14,11 @@ import pandas as pd
 import seaborn as sns
 from sklearn import metrics
 import scipy.io as sio
+from pandas import DataFrame
 import tensorflow as tf
 from tensorflow.python.data import Dataset
 from tensorflow.python.ops import nn
+import threading
 
 time_start = time.time()
 tf.logging.set_verbosity(tf.logging.ERROR)
@@ -24,18 +26,25 @@ pd.options.display.max_rows = 10
 pd.options.display.float_format = '{:.1f}'.format
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = '2'
 
-AD_mat = sio.loadmat('')
-LMCI_mat = sio.loadmat('')
-MCI_mat = sio.loadmat('')
-EMCI_mat = sio.loadmat('')
-HC_mat = sio.loadmat('')
+AD_mat = DataFrame(sio.loadmat('./Data/BCTs/4.AD.mat')['subjects'])
+LMCI_mat = DataFrame(sio.loadmat('./Data/BCTs/3.LMCI.mat')['subjects'])
+MCI_mat = DataFrame(sio.loadmat('./Data/BCTs/2.MCI.mat')['subjects'])
+EMCI_mat = DataFrame(sio.loadmat('./Data/BCTs/1.EMCI.mat')['subjects'])
+HC_mat = DataFrame(sio.loadmat('./Data/BCTs/0.HC.mat')['subjects'])
 
-# Use just the first 10,000 records for training/validation.
-mnist_dataframe = mnist_dataframe.head(10000)
+subjects = pd.concat([AD_mat, LMCI_mat, MCI_mat, EMCI_mat, HC_mat], ignore_index = True)
 
-mnist_dataframe = mnist_dataframe.reindex(np.random.permutation(mnist_dataframe.index))
-mnist_dataframe.head()
 
+subjects = subjects.reindex(np.random.permutation(subjects.index))
+
+class CmdThread(threading.Thread):
+    def __init__(self, cmd):
+        threading.Thread.__init__(self)
+        self.cmd = cmd
+
+    def run(self):
+        print(self.cmd)
+        os.system(self.cmd)
 
 def parse_labels_and_features(dataset):
     """Extracts labels and features.
@@ -44,46 +53,39 @@ def parse_labels_and_features(dataset):
 
     Args:
       dataset: A Pandas `Dataframe`, containing the label on the first column and
-        monochrome pixel values on the remaining columns, in row major order.
+        values on the remaining columns, in row major order.
     Returns:
-      tA `uple` `(labels, features)`:
+      A 'tuple` `(labels, features)`:
         labels: A Pandas `Series`.
         features: A Pandas `DataFrame`.
     """
-    labels = dataset[0]
+    # 这里有两个要求，一是Tensorflow对于label的类型约定是int，float不可以。二是，int的范围需要从range（0，n）连续的
+    labels = dataset[0].astype(int)
 
+    for label_index in labels.index:
+        if labels[label_index] == 1: # for HC
+            labels[label_index] = 0
+        elif labels[label_index] == 10: # for EMCI
+            labels[label_index] = 1
+        elif labels[label_index] == 100: # for MCI
+            labels[label_index] = 2
+        elif labels[label_index] == 1000:# for LMCI
+            labels[label_index] = 3
+        elif labels[label_index] == 10000:  # for AD
+            labels[label_index] = 4
     # DataFrame.loc index ranges are inclusive at both ends.
-    features = dataset.loc[:, 1:784]
-    # Scale the data to [0, 1]
-    # normalization
-    features = features / 255
+
+    features = dataset.loc[:, 1:]
 
     return labels, features
 
 
-training_targets, training_examples = parse_labels_and_features(mnist_dataframe[:7500])
-validation_targets, validation_examples = parse_labels_and_features(mnist_dataframe[7500:10000])
-
-rand_example = np.random.choice(training_examples.index)
-_, ax = plt.subplots()
-ax.matshow(training_examples.loc[rand_example].values.reshape(28, 28))
-ax.set_title("Label: %i" % training_targets.loc[rand_example])
-ax.grid(True)
-
-
-def construct_feature_columns():
-    """Construct the TensorFlow Feature Columns.
-
-    Returns:
-      A set of feature columns
-    """
-
-    # There are 784 pixels in each image.
-    return set([tf.feature_column.numeric_column('pixels', shape=784)])
+training_targets, training_features = parse_labels_and_features(subjects[:100])
+validation_targets, validation_features = parse_labels_and_features(subjects[100:])
 
 
 def create_training_input_fn(features, labels, batch_size, num_epochs=None, shuffle=True):
-    """A custom input_fn for sending MNIST data to the estimator for training.
+    """A custom input_fn for sending AD data to the estimator for training.
 
     Args:
       features: The training features.
@@ -103,7 +105,7 @@ def create_training_input_fn(features, labels, batch_size, num_epochs=None, shuf
         idx = np.random.permutation(features.index)
 
         # 这里为什么新建个字典结构？pixels需要修改
-        raw_features = {"pixels": features.reindex(idx)}
+        raw_features = {"features": features.reindex(idx)}
         raw_targets = np.array(labels[idx])
 
         ds = Dataset.from_tensor_slices((raw_features, raw_targets))  # warning: 2GB limit
@@ -120,7 +122,7 @@ def create_training_input_fn(features, labels, batch_size, num_epochs=None, shuf
 
 
 def create_predict_input_fn(features, labels, batch_size):
-    """A custom input_fn for sending mnist data to the estimator for predictions.
+    """A custom input_fn for sending AD data to the estimator for predictions.
 
     Args:
       features: The features to base predictions on.
@@ -131,7 +133,7 @@ def create_predict_input_fn(features, labels, batch_size):
     """
 
     def _input_fn():
-        raw_features = {"pixels": features.values}
+        raw_features = {"features": features.values}
         raw_targets = np.array(labels)
 
         ds = Dataset.from_tensor_slices((raw_features, raw_targets))  # warning: 2GB limit
@@ -143,144 +145,6 @@ def create_predict_input_fn(features, labels, batch_size):
 
     return _input_fn
 
-
-def train_linear_classification_model(
-        learning_rate,
-        steps,
-        batch_size,
-        training_examples,
-        training_targets,
-        validation_examples,
-        validation_targets):
-    """Trains a linear classification model for the MNIST digits dataset.
-
-    In addition to training, this function also prints training progress information,
-    a plot of the training and validation loss over time, and a confusion
-    matrix.
-
-    Args:
-      learning_rate: An `int`, the learning rate to use.
-      steps: A non-zero `int`, the total number of training steps. A training step
-        consists of a forward and backward pass using a single batch.
-      batch_size: A non-zero `int`, the batch size.
-      training_examples: A `DataFrame` containing the training features.
-      training_targets: A `DataFrame` containing the training labels.
-      validation_examples: A `DataFrame` containing the validation features.
-      validation_targets: A `DataFrame` containing the validation labels.
-
-    Returns:
-      The trained `LinearClassifier` object.
-    """
-
-    periods = 10
-
-    steps_per_period = steps / periods
-    # Create the input functions.
-    predict_training_input_fn = create_predict_input_fn(
-        training_examples, training_targets, batch_size)
-    predict_validation_input_fn = create_predict_input_fn(
-        validation_examples, validation_targets, batch_size)
-    training_input_fn = create_training_input_fn(
-        training_examples, training_targets, batch_size)
-
-    # Create a LinearClassifier object.
-
-    my_optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate)
-    my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
-    classifier = tf.estimator.LinearClassifier(
-        feature_columns=construct_feature_columns(),
-        n_classes=10,
-        optimizer=my_optimizer,
-        config=tf.estimator.RunConfig(keep_checkpoint_max=1)
-    )
-
-    # Train the model, but do so inside a loop so that we can periodically assess
-    # loss metrics.
-    print("Training model...")
-    print("LogLoss error (on validation data):")
-    training_errors = []
-    validation_errors = []
-    for period in range(0, periods):
-        # Train the model, starting from the prior state.
-        classifier.train(
-            input_fn=training_input_fn,
-            steps=steps_per_period
-        )
-
-        # Take a break and compute probabilities.
-        training_predictions = list(classifier.predict(input_fn=predict_training_input_fn))
-        training_probabilities = np.array([item['probabilities'] for item in training_predictions])
-        training_pred_class_id = np.array([item['class_ids'][0] for item in training_predictions])
-        training_pred_one_hot = tf.keras.utils.to_categorical(training_pred_class_id, 10)
-
-        validation_predictions = list(classifier.predict(input_fn=predict_validation_input_fn))
-        validation_probabilities = np.array([item['probabilities'] for item in validation_predictions])
-        validation_pred_class_id = np.array([item['class_ids'][0] for item in validation_predictions])
-        validation_pred_one_hot = tf.keras.utils.to_categorical(validation_pred_class_id, 10)
-
-        # Compute training and validation errors.
-        training_log_loss = metrics.log_loss(training_targets, training_pred_one_hot)
-        validation_log_loss = metrics.log_loss(validation_targets, validation_pred_one_hot)
-        # Occasionally print the current loss.
-        print("  period %02d : %0.2f" % (period, validation_log_loss))
-        # Add the loss metrics from this period to our list.
-        training_errors.append(training_log_loss)
-        validation_errors.append(validation_log_loss)
-    print("Model training finished.")
-    # Remove event files to save disk space.
-    _ = map(os.remove, glob.glob(os.path.join(classifier.model_dir, 'events.out.tfevents*')))
-
-    # Calculate final predictions (not probabilities, as above).
-    final_predictions = classifier.predict(input_fn=predict_validation_input_fn)
-    final_predictions = np.array([item['class_ids'][0] for item in final_predictions])
-
-    accuracy = metrics.accuracy_score(validation_targets, final_predictions)
-    print("Final accuracy (on validation data): %0.2f" % accuracy)
-    time_end = time.time()
-    print('totally cost', (time_end - time_start) / 60)
-    # # Output a graph of loss metrics over periods.
-    # plt.ylabel("LogLoss")
-    # plt.xlabel("Periods")
-    # plt.title("LogLoss vs. Periods")
-    # plt.plot(training_errors, label="training")
-    # plt.plot(validation_errors, label="validation")
-    # plt.legend()
-    # plt.show()
-    #
-    # # Output a plot of the confusion matrix.
-    # cm = metrics.confusion_matrix(validation_targets, final_predictions)
-    # # Normalize the confusion matrix by row (i.e by the number of samples
-    # # in each class).
-    # cm_normalized = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
-    # ax = sns.heatmap(cm_normalized, cmap="bone_r")
-    # ax.set_aspect(1)
-    # plt.title("Confusion matrix")
-    # plt.ylabel("True label")
-    # plt.xlabel("Predicted label")
-    # plt.show()
-
-    return classifier
-
-
-# classifier = train_linear_classification_model(
-#     learning_rate=0.02,
-#     steps=100,
-#     batch_size=10,
-#     training_examples=training_examples,
-#     training_targets=training_targets,
-#     validation_examples=validation_examples,
-#     validation_targets=validation_targets)
-
-# _ = train_linear_classification_model(
-#     learning_rate=0.03,
-#     steps=1000,
-#     batch_size=30,
-#     training_examples=training_examples,
-#     training_targets=training_targets,
-#     validation_examples=validation_examples,
-#     validation_targets=validation_targets)
-
-
 def train_nn_classification_model(
         learning_rate,
         steps,
@@ -290,7 +154,7 @@ def train_nn_classification_model(
         training_targets,
         validation_examples,
         validation_targets):
-    """Trains a neural network classification model for the MNIST digits dataset.
+    """Trains a neural network classification model for the AD  dataset.
 
     In addition to training, this function also prints training progress information,
     a plot of the training and validation loss over time, as well as a confusion
@@ -328,20 +192,24 @@ def train_nn_classification_model(
         training_examples, training_targets, batch_size)
 
     # Create feature columns.
-    feature_columns = [tf.feature_column.numeric_column('pixels', shape=784)]
+    feature_columns = [tf.feature_column.numeric_column('features', shape=training_examples.shape[1])]
 
     # Create a DNNClassifier object.
     my_optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate)
     my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+    # my_optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9)
     classifier = tf.estimator.DNNClassifier(
         feature_columns=feature_columns,
-        n_classes=10,
+        n_classes=5,
         activation_fn=nn.relu,
         hidden_units=hidden_units,
         optimizer=my_optimizer,
         config=tf.contrib.learn.RunConfig(keep_checkpoint_max=1)
     )
-
+    logdir = classifier.model_dir
+    cmd = 'tensorboard --logdir ' + logdir
+    thread = CmdThread(cmd)
+    thread.start()
     # Train the model, but do so inside a loop so that we can periodically assess
     # loss metrics.
     print("Training model...")
@@ -359,12 +227,12 @@ def train_nn_classification_model(
         training_predictions = list(classifier.predict(input_fn=predict_training_input_fn))
         training_probabilities = np.array([item['probabilities'] for item in training_predictions])
         training_pred_class_id = np.array([item['class_ids'][0] for item in training_predictions])
-        training_pred_one_hot = tf.keras.utils.to_categorical(training_pred_class_id, 10)
+        training_pred_one_hot = tf.keras.utils.to_categorical(training_pred_class_id, num_classes = 5)
 
         validation_predictions = list(classifier.predict(input_fn=predict_validation_input_fn))
         validation_probabilities = np.array([item['probabilities'] for item in validation_predictions])
         validation_pred_class_id = np.array([item['class_ids'][0] for item in validation_predictions])
-        validation_pred_one_hot = tf.keras.utils.to_categorical(validation_pred_class_id, 10)
+        validation_pred_one_hot = tf.keras.utils.to_categorical(validation_pred_class_id, num_classes = 5)
 
         # Compute training and validation errors.
         training_log_loss = metrics.log_loss(training_targets, training_pred_one_hot)
@@ -376,7 +244,8 @@ def train_nn_classification_model(
         validation_errors.append(validation_log_loss)
     print("Model training finished.")
     # Remove event files to save disk space.
-    _ = map(os.remove, glob.glob(os.path.join(classifier.model_dir, 'events.out.tfevents*')))
+
+    # _ = map(os.remove, glob.glob(os.path.join(classifier.model_dir, 'events.out.tfevents*')))
 
     # Calculate final predictions (not probabilities, as above).
     final_predictions = classifier.predict(input_fn=predict_validation_input_fn)
@@ -409,21 +278,14 @@ def train_nn_classification_model(
     return classifier
 
 
-# https://stackoverflow.com/questions/48824351/pycharm-tensorflow-warning-type-variable-doesnt-have-expected-attribute
-# The - operator is called on var inside variable_summaries:
-# stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-# Normally Python would be looking for the __sub__ method of var when evaluating the expression var - mean. However, a tf.Variable instance is wrapped around state_ops.variable_op_v2 in order to support Ops on GPU/CPU and does not have a __sub__ method that Python would normally expect.
-# Therefore, this warning is inherent to the way TensorFlow heavily customizing standard Python operators on Tensorflow objects in order to support the expressions we are used to while enabling GPU/CPU computation with Tensorflow OPs.
-# I'd say you can safely ignore this warning on any Tensorflow object. Unfortunately I don't know how you can suppress this warning in PyCharm.
-
 classifier = train_nn_classification_model(
-    learning_rate=0.05,
-    steps=1000,
-    batch_size=30,
-    hidden_units=[100, 100],
-    training_examples=training_examples,
+    learning_rate=0.001,
+    steps=100000,
+    batch_size=1,
+    hidden_units=[1000, 1000, 1000],
+    training_examples=training_features,
     training_targets=training_targets,
-    validation_examples=validation_examples,
+    validation_examples=validation_features,
     validation_targets=validation_targets)
 
 
